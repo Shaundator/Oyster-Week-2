6bb287aad22d043788361258db89ab42c145ed79
Rasha <rash0081@kea.stud.dk>
2022-02-15 11:41:31 +0100

    commit3

--- src/demo/NumberChecker.java
+++ src/demo/NumberChecker.java
@@ -7,8 +7,27 @@ public class NumberChecker {
         if(line.matches("\\d+")){
             return true;
         }
-
-
+        if(line.endsWith("-")||line.endsWith(".")||line.endsWith("e")){
+            return false;
+        }
+        String numbers = "";
+        String dataPoints = "";
+        boolean validData = false;
+        for(int i = 0; i < line.length(); i++){
+            if(line.substring(i,(i+1)).matches("\\d+")){
+                numbers += line.substring(i,(i+1));
+            } else {
+                dataPoints += line.substring(i,(i+1));
+            }
+        }
+        if(dataPoints.equals("-.")||dataPoints.equals("-e")||dataPoints.equals("-")||dataPoints.equals("e")||dataPoints.equals(".")){
+            validData = true;
+        }
+        if(validData){
+            if(numbers.matches("\\d+")){
+                return true;
+            }
+        }
         return false;
     }
 }

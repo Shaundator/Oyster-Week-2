6834d7a040d916a961b564b3351668a3ae903c01
Rasha <rash0081@kea.stud.dk>
2022-02-15 11:31:52 +0100

    commit2

--- src/demo/NumberChecker.java
+++ src/demo/NumberChecker.java
@@ -7,26 +7,8 @@ public class NumberChecker {
         if(line.matches("\\d+")){
             return true;
         }
-        String[] data = line.split("\\d+");
-        String dataString = "";
-        for(int i = 0; i < data.length; i++){
-            System.out.println("data " + (i+1) + " " + data[i]);
-            dataString += data[i];
-        }
-        System.out.println("data string : " + dataString);
 
 
-
-        switch(data.length){
-            case 1:
-                System.out.println("case 1");
-                break;
-            case 2:
-                System.out.println("case 2");
-                break;
-            default:
-                System.out.println("case >2");
-        }
         return false;
     }
 }
